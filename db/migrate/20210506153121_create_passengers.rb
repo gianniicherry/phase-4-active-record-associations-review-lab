4f34b1929a68854391722f539f1d05be45c191fe
Gianni <gianniicherry@gmail.com>
2023-06-02 09:50:33 -0700

    commit tree fbf88cfb2792193594dfc94467bf6ef7329b833f

--- db/migrate/20210506153121_create_passengers.rb
+++ db/migrate/20210506153121_create_passengers.rb
@@ -3,6 +3,7 @@ class CreatePassengers < ActiveRecord::Migration[6.1]
     create_table :passengers do |t|
 
       t.timestamps
+      
     end
   end
 end
--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -3,6 +3,8 @@ class CreateRides < ActiveRecord::Migration[6.1]
     create_table :rides do |t|
 
       t.timestamps
+      t.integer "taxi_id"
+      t.integer "passenger_id"
     end
   end
 end
